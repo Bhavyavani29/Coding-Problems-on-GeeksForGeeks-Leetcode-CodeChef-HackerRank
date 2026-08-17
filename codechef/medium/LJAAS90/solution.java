f7c10b3d1308d4a6a0a5f760334e73ed4f669d23
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 19:58:08 +0530

    ✅ LJAAS90 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/LJAAS90/solution.java
+++ codechef/medium/LJAAS90/solution.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
-
 class Codechef
 {
 	public static void main (String[] args) 
 	{
-        int a = (5 + 7); 
-        System.out.println(a);
+        int choice = 2;
+        switch(choice){
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Option 3 selected");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
 
 	}
 }
