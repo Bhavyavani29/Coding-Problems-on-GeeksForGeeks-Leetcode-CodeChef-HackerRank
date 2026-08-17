247ce86016f877b4c337d090b41522f3f66799e3
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 10:09:18 +0530

    ✅ LJAAS79 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/LJAAS79/solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        switch (ch) {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
