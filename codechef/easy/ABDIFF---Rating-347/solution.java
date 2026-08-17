becb5c41316436dca06f6d55b09b7da79c97b93a
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 19:48:41 +0530

    ✅ ABDIFF - Rating 347 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/easy/ABDIFF---Rating-347/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		// write your code here
+		int x = a + b;
+		int y = a * b;
+		if(y >= x){
+		    System.out.println(y - x);
+		}
+		else{
+		    System.out.println(x - y);
+		}
+	}
+}
