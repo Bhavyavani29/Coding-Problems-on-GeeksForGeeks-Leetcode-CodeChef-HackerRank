362d0ae5984b614c04323956a18ba91b022acf58
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 19:52:10 +0530

    ✅ LJAAS87 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/LJAAS87/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int number = 19;
+        System.out.println(number);
+
+	}
+}
