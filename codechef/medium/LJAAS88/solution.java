aff27d2085ce7bfddc423f5a6d98760375ddc133
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 19:54:10 +0530

    ✅ LJAAS88 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/LJAAS88/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        int n;
+        n = 8;
+        String s = "CodeChef";
+        System.out.println(s.charAt(n-1));
+
+	}
+}
