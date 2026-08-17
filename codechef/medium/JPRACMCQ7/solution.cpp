94db1cb021ecaee1ffa19f9cfb843fda2451e079
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 10:29:26 +0530

    ✅ JPRACMCQ7 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/JPRACMCQ7/solution.cpp
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// write your code here
+		int blackCells = (n * n) / 2;
+		System.out.println(blackCells);
+	}
+}
