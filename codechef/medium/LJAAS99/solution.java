6d3ac98635fd2bdc0df6c6f71e60ace256d9ccbe
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 19:59:24 +0530

    ✅ LJAAS99 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/LJAAS99/solution.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] array = {10, 12, 25, 9, 20};
+        // Complete the code 
+        int ans = array[0] * array[2];
+        System.out.println(ans);
+	}
+}
