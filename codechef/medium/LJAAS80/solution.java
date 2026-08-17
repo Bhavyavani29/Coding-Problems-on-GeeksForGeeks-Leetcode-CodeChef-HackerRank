97512a08c5ed3997d4c67011a457c7296bcfeb57
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-17 10:07:19 +0530

    ✅ LJAAS80 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ codechef/medium/LJAAS80/solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //your code goes InheritableThreadLocal
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        char op = sc.next().charAt(0);
+        switch (op) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
