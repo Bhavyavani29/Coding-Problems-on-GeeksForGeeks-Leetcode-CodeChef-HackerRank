ffdd06e6a3d97100f1c63fcdfbf93d90aef11037
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-18 22:26:18 +0530

    ✅ LJAAS119 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/LJAAS119/solution.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int sum = 0;
+        for(int i = 1; i <= 10; i++){
+            sum = sum + i;
+        }
+        System.out.print(sum);
+	}
+}
