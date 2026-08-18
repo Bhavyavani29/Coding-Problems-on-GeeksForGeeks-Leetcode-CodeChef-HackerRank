789591578bfa21aa6d9d8f56a137148a9895108c
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-18 22:16:35 +0530

    ✅ LJAAS109 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/LJAAS109/solution.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		//Scanner sc = new Scanner(System.in);
+		 int x = 1;
+		 while(x <= 5){
+		     System.out.print(x * x + " ");
+		     x = x + 1;
+		 }
+
+	}
+}
