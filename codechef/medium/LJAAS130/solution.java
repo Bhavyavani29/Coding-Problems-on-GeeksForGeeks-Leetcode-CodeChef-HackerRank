9c378a133cd2c11e8645ffa47c8a396ea373b4b2
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-20 22:56:19 +0530

    ✅ LJAAS130 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-20

---
+++ codechef/medium/LJAAS130/solution.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] values = {2, 10, 12, 6, 15};
+        // Complete the code 
+        for(int num : values){
+            if(num > 10)
+                continue;
+            System.out.print(num * num + " ");
+        }
+    }
+}
