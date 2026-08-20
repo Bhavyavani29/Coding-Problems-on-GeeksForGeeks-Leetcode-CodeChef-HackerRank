1066937a5e7335b15a8d9f0bae61981ae675890a
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-20 22:53:51 +0530

    ✅ LJAAS129 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-20

---
+++ codechef/medium/LJAAS129/solution.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] numbers = {2, 4, 8, 12, 8};
+        // Complete the code
+        for(int i = 0;i < numbers.length;i++){
+            if(numbers[i] == 8){
+                System.out.println(i);
+                break;
+            }
+        }
+	}
+}
