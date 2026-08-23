ae0acbc7592d2c5e4e6f096b4030a86a2b965e44
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-23 21:54:21 +0530

    ✅ LJAAS140 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-23

--- codechef/medium/LJAAS140/solution.java
+++ codechef/medium/LJAAS140/solution.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int base = scanner.nextInt();
-        int exponent = scanner.nextInt();
-        
-        int result = calculatePower(base, exponent);
-        System.out.println(result);    
+        // Take length and width as input
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int width = sc.nextInt();
+        int area = calculateArea(length, width);
+        System.out.println(area);    
     }
     
-    public static int calculatePower(int base, int exponent) {
-        // Complete the method 
-        return (int) Math.pow(base, exponent);
+    public static int calculateArea(int length, int width) {
+        // Complete this method
+        return (length * width);
     }
 }
