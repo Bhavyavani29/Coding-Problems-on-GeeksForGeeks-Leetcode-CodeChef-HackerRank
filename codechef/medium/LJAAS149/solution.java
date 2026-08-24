eadcd83dfa4bff781c89f83dd4fbae7fe31bc356
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-24 21:10:10 +0530

    ✅ LJAAS149 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/LJAAS149/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t --> 0){
+		    int N = sc.nextInt();
+		     System.out.println(N  + 1);
+		}
+	}
+}
