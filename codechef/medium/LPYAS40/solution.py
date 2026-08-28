03e39f6e4ebe72d971a28ab8c82a0ae99ca9facd
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-28 22:38:16 +0530

    ✅ LPYAS40 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-28

---
+++ codechef/medium/LPYAS40/solution.py
@@ -0,0 +1,9 @@
+# cook your dish here
+one = "Coding"
+two = "on"
+three = "CodeChef"
+four = one + " " + two + " " + three
+print(one + " - " + str(len(one)))
+print(two + " - " + str(len(two)))
+print(three + " - " + str(len(three)))
+print(four + " - " + str(len(four)))
