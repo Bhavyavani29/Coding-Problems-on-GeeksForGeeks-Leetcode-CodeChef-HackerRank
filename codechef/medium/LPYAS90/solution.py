1d5f8f025a7efc3d7849c0ffbc8beb0da019fe2a
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-05 23:17:16 +0530

    ✅ LPYAS90 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/medium/LPYAS90/solution.py
+++ codechef/medium/LPYAS90/solution.py
@@ -1,2 +1,11 @@
-a = 4 + 8
-print(a)
+# Update the code below to debug the problem
+choice = int(input())
+
+if choice == 1:
+    print("Option 1 selected")
+elif choice == 1:
+    print("Option 2 selected")
+elif choice == 3:
+    print("Option 3 selected")
+else:
+    print("Invalid choice")
