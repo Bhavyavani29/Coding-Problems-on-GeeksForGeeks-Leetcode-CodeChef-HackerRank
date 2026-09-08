b77ceec844d9b2b146ddf315cf8557c2dc59b057
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-08 22:45:06 +0530

    ✅ LPYAS130 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-08

--- codechef/medium/LPYAS130/solution.py
+++ codechef/medium/LPYAS130/solution.py
@@ -1,6 +1,6 @@
-numbers = list(map(int, input().split()))
+values = list(map(int, input().split()))
 # Update your code below this line
-for i, num in enumerate(numbers):
-    if num == 8:
-        print(i)
-        break
+for i, num in enumerate(values):
+    if num > 10:
+        continue
+    print(num * num)
