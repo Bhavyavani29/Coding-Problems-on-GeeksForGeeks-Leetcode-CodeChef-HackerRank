e7fe6a82fd790f714e118d5519f7e7acb13e4fd8
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-14 21:49:14 +0530

    ✅ LPYAS150 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/LPYAS150/solution.py
+++ codechef/medium/LPYAS150/solution.py
@@ -1,14 +1,9 @@
-def is_even(num):
-    return num % 2 == 0
-
 def main():
     t = int(input())
-    for _ in range(t):
-        num = int(input())
-        if is_even(num):
-            print("Even")
-        else:
-            print("Odd")
+    while t > 0:
+        N = int(input())
+        print(N + 1)
+        t -= 1
 
 if __name__ == "__main__":
     main()
