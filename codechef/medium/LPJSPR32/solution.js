1f29e4e3eb00229dbc2123e12e849a766ed9cf52
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-17 22:58:58 +0530

    ✅ LPJSPR32 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/LPJSPR32/solution.js
+++ codechef/medium/LPJSPR32/solution.js
@@ -1,4 +1,4 @@
 // your code goes here
-let temperature = 25.5;
-console.log("Celsius - ", temperature);
-console.log("Kelvin - ", temperature + 273);
+let hour = 5;
+console.log(5 * 60);
+console.log(5 * 60 * 60);
