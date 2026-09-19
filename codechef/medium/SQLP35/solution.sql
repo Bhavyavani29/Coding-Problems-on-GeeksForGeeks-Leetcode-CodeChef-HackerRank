4fd8e3f3b8e628f8561bdffcba38a047f5555e21
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-19 11:08:56 +0530

    ✅ SQLP35 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/medium/SQLP35/solution.sql
+++ codechef/medium/SQLP35/solution.sql
@@ -1,5 +1,2 @@
-SELECT property_id, title, location, rating,
-       (SELECT AVG(rating) FROM Properties) AS avg_all_ratings
-FROM Properties
-WHERE rating > (SELECT AVG(rating) FROM Properties)
-ORDER BY property_id ASC;
+-- your code goes here
+select user_id, count(property_id) as favorite_count from Favorites group by user_id order by count(property_id) desc limit 1;
