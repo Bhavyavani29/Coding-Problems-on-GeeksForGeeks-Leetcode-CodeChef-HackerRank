99dea1589bcfe1700cb052a17cb0fc2d2db5fa35
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-20 22:14:44 +0530

    ✅ LPJSPR43 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/LPJSPR43/solution.js
@@ -0,0 +1,10 @@
+// complete the following code
+
+let one = 'Coding';
+let two = 'on';
+let three = 'CodeChef';
+let combination = one + ' ' + two + ' ' + three;
+console.log(one + ' - ' + one.length);
+console.log(two + ' - ' + two.length);
+console.log(three + ' - ' + three.length);
+console.log(combination + ' - ' + combination.length);
