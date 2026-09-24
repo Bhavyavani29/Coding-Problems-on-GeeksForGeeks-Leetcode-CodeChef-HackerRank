22395e49379a06ab8ae1160662f07cc163401efe
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-24 22:14:00 +0530

    ✅ LPJSPR76 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ codechef/medium/LPJSPR76/solution.js
@@ -0,0 +1,9 @@
+  let x = parseInt(inputChar);
+  
+    if (x > 0) {
+        console.log("Positive");
+    } else if (x < 0) {
+        console.log("Negative");
+    } else {
+        console.log("Zero");
+    }
